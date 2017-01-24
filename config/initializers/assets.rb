576c9a77557e1498207757c6be8bc42defd12ecf
murilohns <murilohns@gmail.com>
2017-01-24 11:04:01 -0200

    Fixed Bootstrap Library Bugs

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( application.css, site. css, backoffice.css )
+
+Rails.application.config.assets.precompile += %w( application.js, site.js, backoffice.js )
