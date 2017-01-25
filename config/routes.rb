a2b340f0e12cbb499ff51ec0bd080cb5fc1c8cbd
Murilo Henrique <murilohns@gmail.com>
2017-01-25 10:58:23 -0200

    Fixed bug in Dropdown-menu

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  namespace :site do
+    get 'site', to: 'home#index'
+  end
+
+  namespace :backoffice do
+    get 'dashboard', to: 'dashboard#index'
+  end
+
+  devise_for :admins
+  devise_for :members
+
+  root 'site/home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
